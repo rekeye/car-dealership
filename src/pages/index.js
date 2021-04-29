b50699f8c0a240dff66694350258505d2c9f75f2
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-04-29 18:37:24 +0200

    cleaning content on placeholder pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,6 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Strona główna" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
   </Layout>
 )
 
