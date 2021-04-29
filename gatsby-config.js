6a358611598c78be78678bb94222cc3c28b0fc69
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-04-29 17:14:48 +0200

    changing gatsby-config data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Auto Komis`,
+    description: `Cillum laboris exercitation eu labore eu duis reprehenderit elit officia cillum reprehenderit.`,
+    author: `@rekeye`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,18 +19,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `car-dealership`,
+        short_name: `car-dealer`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: ``, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
