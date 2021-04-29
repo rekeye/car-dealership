20f41495adbc36bbf615c7f94aacb369cc8c4bd6
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-04-29 18:35:24 +0200

    adding styled component elements in header and layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,16 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { Link } from "gatsby"
+
+import styled from "styled-components"
+import { StyledLink } from "./styledComponents"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header>
+    <div>
+      <h1>
+        <StyledLink to="/" color="white">
           {siteTitle}
-        </Link>
+        </StyledLink>
       </h1>
     </div>
   </header>
@@ -36,7 +21,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: '',
 }
 
 export default Header
