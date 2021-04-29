9bb7afc825426f34f07d00f0069244bec014116a
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-04-29 21:53:53 +0200

    adding content to contact.js and offers.js

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const ContactPage = () => (
+  <Layout>
+    <Seo title="Kontakt" />
+  </Layout>
+)
+
+export default ContactPage
---
+++ src/pages/offers.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const OffersPage = () => (
+  <Layout>
+    <Seo title="Oferty" />
+  </Layout>
+)
+
+export default OffersPage
