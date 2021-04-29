96c770ceaad02a838f5023711af7b5364660342d
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-04-29 18:59:03 +0200

    styling header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,30 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import { StyledLink } from "./styledComponents"
+
+const StyledHeaderDiv = styled.div`
+  background: ${props => (props.colored && css`var(--base-dark-red)`)};
+  padding: 2em 4em;
+`
+const StyledSiteTitle = styled.h1`
+  font-weight: 300;
+  font-size: 2.5rem;
+`
 
 const Header = ({ siteTitle }) => (
   <header>
-    <div>
-      <h1>
+    <StyledHeaderDiv colored>
+      <StyledSiteTitle>
         <StyledLink to="/" color="white">
           {siteTitle}
         </StyledLink>
-      </h1>
-    </div>
+      </StyledSiteTitle>
+    </StyledHeaderDiv>
+    <StyledHeaderDiv>
+asdadsad
+    </StyledHeaderDiv>
   </header>
 )
 
