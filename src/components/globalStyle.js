0922159a74f8b4d41f752e5a12b7dfdd0e988ac2
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-04-29 18:43:34 +0200

    adding css variables of cloros to globalStyle

--- src/components/globalStyle.js
+++ src/components/globalStyle.js
@@ -3,6 +3,12 @@ import { createGlobalStyle } from "styled-components"
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Lato&family=Roboto:wght@100;300;400&display=swap');
 
+    :root {
+        --base-dark-red: #CA0305;
+        --base-dark-blue: #00249F;
+        --black: #1A1A1A;
+        --white: #F5F5F5;
+    }
     * {
         box-sizing: border-box;
         margin: 0;
