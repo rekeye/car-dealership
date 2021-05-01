da56c45b009206d1cc431a1dd889e45be3253568
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-05-01 14:28:31 +0200

    resetting

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { ApolloProvider } from 'react-apollo'
-import { client } from './src/apollo/client'
-
-export const wrapRootElement = ({ Root }) => (
-    <ApolloProvider client={client}>
-        <Root />
-    </ApolloProvider>
-)
