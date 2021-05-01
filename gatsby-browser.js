b9885c0285f9e91a1d8f8c9509dfe10fe35506f0
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-05-01 14:16:47 +0200

    add root wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
+import React from 'react'
+import { ApolloProvider } from 'react-apollo'
+import { client } from './src/apollo/client'
 
-// You can delete this file if you're not using it
+export const wrapRootComponent = ({ Root }) => (
+    <ApolloProvider client={client}>
+        <Root />
+    </ApolloProvider>
+)
--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,12 +1,10 @@
 import fetch from 'isomorphic-fetch'
-import {ApolloClient, HttpLink, InMemoryCache} from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
         uri: 'https://api-eu-central-1.graphcms.com/v2/cko1dd38yu4qc01yz84lf9k6z/master',
         fetch
     })
 })
-
-export default client
