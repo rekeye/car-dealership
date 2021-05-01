e502534f1fa1d0bbd82d171cc8bb7581769d87ed
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-05-01 14:11:41 +0200

    create apollo client

---
+++ src/apollo/client.js
@@ -0,0 +1,12 @@
+import fetch from 'isomorphic-fetch'
+import {ApolloClient, HttpLink, InMemoryCache} from '@apollo/client'
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: 'https://api-eu-central-1.graphcms.com/v2/cko1dd38yu4qc01yz84lf9k6z/master',
+        fetch
+    })
+})
+
+export default client
