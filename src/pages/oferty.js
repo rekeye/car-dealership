2118a3391d904d1ef1d3f91343b34e391abe605c
Szymon Paluch <12szymon.paluch34@gmail.com>
2021-05-06 13:07:33 +0200

    offers page search cors error resolved

--- src/pages/oferty.js
+++ src/pages/oferty.js
@@ -10,21 +10,21 @@ import CustomHits from "../components/search/CustomHits"
 
 
 const client = algoliasearch(
-  process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_SEARCH_KEY
+  process.env.GATSBY_ALGOLIA_APP_ID,
+  process.env.GATSBY_ALGOLIA_API_KEY
 )
 
 const OffersPage = () => (
-    <Layout>
-      <InstantSearch searchClient={client} indexName="car_dealership">
-        <Seo title="Oferty" />
-        <StyledSearchBackgroundImage/>
-        <SectionTitle $padding>Wszystkie oferty: </SectionTitle>
-        <PostsContainer>
-          <CustomHits />
-        </PostsContainer>
-      </InstantSearch>
-    </Layout>
-  )
+  <Layout>
+    <InstantSearch searchClient={client} indexName="car_dealership">
+      <Seo title="Oferty" />
+      <StyledSearchBackgroundImage/>
+      <SectionTitle $padding>Wszystkie oferty: </SectionTitle>
+      <PostsContainer>
+        <CustomHits />
+      </PostsContainer>
+    </InstantSearch>
+  </Layout>
+)
 
 export default OffersPage
